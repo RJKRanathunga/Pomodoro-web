53d69730a20696f7fae8aeba45e33f4b4f1f6bb6
RJKRanathunga <oshadawork@gmail.com>
2025-01-09 14:52:38 +0530

    added redis.ping() again

--- pages/api/saveToken.js
+++ pages/api/saveToken.js
@@ -31,6 +31,7 @@ export default async function handler(req, res) {
     }
   } else if (req.method === 'GET') {
     if (tokens.length === 0) {
+      await redis.ping();
       const storedTokens = await redis.get('FCM_tokens');
       return res.status(200).json(JSON.parse(storedTokens));
     } else {
