239c251cdcfbccd09a61b8d8772d179d4b86015e
RJKRanathunga <oshadawork@gmail.com>
2025-02-01 16:02:10 +0530

    feat: add loading state to overlay and display loading indicator during data fetch

--- app/secondary screens/Report/Overlay.js
+++ app/secondary screens/Report/Overlay.js
@@ -47,10 +47,17 @@ const Overlay = ({ isOverlayVisible, toggleOverlay }) => {
   // Outside click handler - end
   
   const [workTimeSegments_for7days, setWorkTimeSegments_for7days] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    fetchLast7DaysData().then((data) => {
-      setWorkTimeSegments_for7days(data);
-    })},[isOverlayVisible]);
+    if (isOverlayVisible) {
+      setIsLoading(true);
+      fetchLast7DaysData().then((data) => {
+        setWorkTimeSegments_for7days(data);
+        setIsLoading(false);
+      });
+    }
+  }, [isOverlayVisible]);
 
   // const workTimeSegments_for7days = {
   //   dayReport_20250114: null,
@@ -183,83 +190,89 @@ const Overlay = ({ isOverlayVisible, toggleOverlay }) => {
     isOverlayVisible && (
       <div className="overlay">
         <div className="overlay-content" ref={overlayRef}>
-        <div className="tabs">
-            <button className={`tab-button ${activeTab === 'report' ? 'active' : ''}`} onClick={() => setActiveTab('report')}>Report</button>
-            <button className={`tab-button ${activeTab === 'summary' ? 'active' : ''}`} onClick={() => setActiveTab('summary')}>Summary</button>
-          <GrClose className="report-close-icon" onClick={toggleOverlay} />
-        </div>
-        <hr className="report-divider" />
-        {activeTab === 'report' && (
-          <div className="timeline-container">
-            {Object.keys(workTimeSegments_for7days).map((key) => {
-
-              const reportDay = key.split('_')[1];
-              const todayDay = currentTime.toISOString().split('T')[0].split('-').join('');
-              const workTimeSegments = workTimeSegments_for7days[key];
-              const workTimes = workTimeSegments ? workTimeSegments.map(({ start, end }) => {
-                if (end === 0) {
-                  const endTime =reportDay===todayDay ? ( currentMinutes < start + 25 ? currentMinutes:start )  : start;
-                  return { start, end: endTime };
-                }
-                return { start, end };
-              }) : [];
-
-              const totalWorkMinutes = workTimes.reduce((sum, { start, end }) => sum + (end - start), 0);
-              const totalWorkHours = Math.floor(totalWorkMinutes / 60);
-              const remainingMinutes = totalWorkMinutes % 60;
-
-              return(
-                <div key={key} className='day-report'>
-                  <h3>{getDate_byKey(key)}</h3>
-                  {workTimeSegments ? (
-                    <div>
-                      <div className="timeline">
-                      {Array.from({ length: totalMinutesInDay }, (_, index) => {
-                        const isWorkTime = workTimes.some(
-                          (workTime) => index >= workTime.start && index <= workTime.end
-                        );
-                        return (
-                          <div
-                            key={index}
-                            className={`timeline-segment ${isWorkTime ? 'work' : 'break'}`}
-                          ></div>
-                        );
-                      })}
-                      {Array.from({ length: 25 }, (_, index) => (
-                        <div key={index} className="timeline-hour-line" style={{ left: `${(index / 24) * 100}%` }}></div>
-                      ))}
-                      {reportDay === todayDay && <div className="current-time-marker" style={{ left: `${currentTimePosition}%` }}></div>}
-                    </div>
-                    <div className="timeline-hours">
-                      {Array.from({ length: 25 }, (_, index) => (
-                        <div key={index} className="timeline-hour">
-                          {index}:00
-                        </div>
-                      ))}
-                    </div>
-                    <div className="total-work-time">
-                      Total Work Time Today: {totalWorkHours}h {remainingMinutes}m
-                    </div>
-                  </div>
-                  ):(
-                    <h3>No data</h3>
-                  )}
-                </div>
-              );
-            })}
+        {isLoading?(
+          <div className="loading">Loading...</div>
+        ):(
+        <div>
+          <div className="tabs">
+              <button className={`tab-button ${activeTab === 'report' ? 'active' : ''}`} onClick={() => setActiveTab('report')}>Report</button>
+              <button className={`tab-button ${activeTab === 'summary' ? 'active' : ''}`} onClick={() => setActiveTab('summary')}>Summary</button>
+            <GrClose className="report-close-icon" onClick={toggleOverlay} />
           </div>
-        )}
-        {activeTab === 'summary' && (
-            <div className="summary-container">
-              <div className="summary-box">
-                <div className="summary-time">
-                  <h1>{summery_FullWorkHours || "00"} : {summery_remainingMinutes || "00"}</h1>
+          <hr className="report-divider" />
+          {activeTab === 'report' && (
+            <div className="timeline-container">
+              {Object.keys(workTimeSegments_for7days).map((key) => {
+
+                const reportDay = key.split('_')[1];
+                const todayDay = currentTime.toISOString().split('T')[0].split('-').join('');
+                const workTimeSegments = workTimeSegments_for7days[key];
+                const workTimes = workTimeSegments ? workTimeSegments.map(({ start, end }) => {
+                  if (end === 0) {
+                    const endTime =reportDay===todayDay ? ( currentMinutes < start + 25 ? currentMinutes:start )  : start;
+                    return { start, end: endTime };
+                  }
+                  return { start, end };
+                }) : [];
+
+                const totalWorkMinutes = workTimes.reduce((sum, { start, end }) => sum + (end - start), 0);
+                const totalWorkHours = Math.floor(totalWorkMinutes / 60);
+                const remainingMinutes = totalWorkMinutes % 60;
+
+                return(
+                  <div key={key} className='day-report'>
+                    <h3>{getDate_byKey(key)}</h3>
+                    {workTimeSegments ? (
+                      <div>
+                        <div className="timeline">
+                        {Array.from({ length: totalMinutesInDay }, (_, index) => {
+                          const isWorkTime = workTimes.some(
+                            (workTime) => index >= workTime.start && index <= workTime.end
+                          );
+                          return (
+                            <div
+                              key={index}
+                              className={`timeline-segment ${isWorkTime ? 'work' : 'break'}`}
+                            ></div>
+                          );
+                        })}
+                        {Array.from({ length: 25 }, (_, index) => (
+                          <div key={index} className="timeline-hour-line" style={{ left: `${(index / 24) * 100}%` }}></div>
+                        ))}
+                        {reportDay === todayDay && <div className="current-time-marker" style={{ left: `${currentTimePosition}%` }}></div>}
+                      </div>
+                      <div className="timeline-hours">
+                        {Array.from({ length: 25 }, (_, index) => (
+                          <div key={index} className="timeline-hour">
+                            {index}:00
+                          </div>
+                        ))}
+                      </div>
+                      <div className="total-work-time">
+                        Total Work Time Today: {totalWorkHours}h {remainingMinutes}m
+                      </div>
+                    </div>
+                    ):(
+                      <h3>No data</h3>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          )}
+          {activeTab === 'summary' && (
+              <div className="summary-container">
+                <div className="summary-box">
+                  <div className="summary-time">
+                    <h1>{summery_FullWorkHours.toString().padStart(2, '0')} : {summery_remainingMinutes.toString().padStart(2, '0')}</h1>
+                  </div>
+                  <div className="summary-text">
+                    <h4>Total work in last week</h4>
+                  </div>
                 </div>
-                <div className="summary-text">
-                  <h4>Total work in last week</h4>
-                </div>
+                <Line data={chartData} options={options} />
               </div>
-              <Line data={chartData} options={options} />
+            )}
             </div>
           )}
         </div>
